1a8b2a8278389bf97a0aef344e4b4845b42e7ac3
Liza-liza <lizz4mail@gmail.com>
2017-10-20 22:57:17 +0300

    update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,25 @@
 import json
-
+import sys
+from pprint import pprint
 
 def load_data(filepath):
-    pass
-
+    try:
+       with open(filepath,'r') as f:
+	       MyData = json.load(f)
+    except FileNotFoundError as err:
+        print(err)
+        return None
+    else:
+        return MyData
 
 def pretty_print_json(data):
-    pass
+    pprint(data)
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+       data = load_data(sys.argv[1])
+       if data:
+           pretty_print_json(data)
+    else:
+        print("Using: python3 pprint_json.py <path to file>")
