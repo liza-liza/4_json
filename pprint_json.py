4290c702a9195d99f8174198c75b169ea6b3dce5
Liza-liza <lizz4mail@gmail.com>
2017-10-21 13:00:40 +0300

    update for validate JSON output

--- pprint_json.py
+++ pprint_json.py
@@ -1,19 +1,17 @@
 import json
 import sys
-from pprint import pprint
+
 
 def load_data(file_path):
     try:
-        with open(file_path,'r') as f:
-	        json_data = json.load(f)
+        with open(file_path, 'r') as f:
+            return json.load(f)
     except FileNotFoundError as err:
-        print(err)
         return None
-    else:
-        return json_data
+
 
 def pretty_print_json(json_data):
-    pprint(json_data)
+    print(json.dumps(json_data, indent=4,ensure_ascii=False, sort_keys=True))
 
 
 if __name__ == '__main__':
@@ -21,5 +19,8 @@ if __name__ == '__main__':
         json_data = load_data(sys.argv[1])
         if json_data:
             pretty_print_json(json_data)
+        else:
+            print("File or directory {} not found".format(sys.argv[1]))
     else:
         print("Using: python3 pprint_json.py <path to file>")
+
